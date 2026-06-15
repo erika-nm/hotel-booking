cc4aeb7566ec23fddb6aec77b5155de0fc08a0ed
alibaba281017 <alibaba281017@gmail.com>
2026-06-15 14:54:18 +0000

    connect tidb

--- hotel-booking/hotel-booking/backend/db.js
+++ hotel-booking/hotel-booking/backend/db.js
@@ -1,15 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "hotel_booking",
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 db.connect((err) => {
   if (err) {
-    console.log(err);
+    console.log("DB Error:", err);
   } else {
     console.log("Database Connected");
   }
