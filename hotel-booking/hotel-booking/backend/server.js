ba080013e1116e75359ce8ce7b9e50ef3fa151e1
alibaba281017 <alibaba281017@gmail.com>
2026-06-16 10:35:27 +0000

    add missing dependencies

--- hotel-booking/hotel-booking/backend/server.js
+++ hotel-booking/hotel-booking/backend/server.js
@@ -59,8 +59,8 @@ app.get("/db-name", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
